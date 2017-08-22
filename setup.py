3bb51fcc0813b1b8c477c75837fbbfaacbd34f09
AdriNicolau <anicolau@gisce.net>
2017-08-22 15:09:20 +0200

    Updated the dependencies.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url='https://github.com/gisce/flask-login-openerp',
     license='MIT',
     author='GISCE-TI, S.L.',
-    install_requires=['Flask', 'Flask-Login', 'Flask-OpenERP', 'Flask-WTF'],
+    install_requires=['Flask', 'Flask-Login', 'Flask-erppeek', 'Flask-WTF'],
     include_package_data=True,
     author_email='ti@gisce.net',
     description='OpenERP Login for Flask using Flask-Login'
