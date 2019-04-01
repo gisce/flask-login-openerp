4da04c354bdcaf57df488d99c358a1ec6478c15f
Xavier Barnada <xbarnada@gmail.com>
2019-04-01 09:58:22 +0200

    Bump to v0.5.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask-login-openerp',
-    version='0.5.3',
+    version='0.5.4',
     packages=find_packages(),
     url='https://github.com/gisce/flask-login-openerp',
     license='MIT',
